c9d0ac2115a3f2020b5c1290d96d74cd84e26819
lulavalenca <lulavalencafilho@gmail.com>
2025-06-25 16:40:21 -0300

    adicionando detalhes do carro na página de detalhes

--- cars/views.py
+++ cars/views.py
@@ -2,11 +2,11 @@
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
-
-# Create your views here.
 
 class CarsListView(ListView):
     model = Car
@@ -20,7 +20,7 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
     
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
@@ -32,6 +32,7 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -39,7 +40,8 @@ class CarUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
-    
+
+@method_decorator(login_required(login_url='login'), name='dispatch')    
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
